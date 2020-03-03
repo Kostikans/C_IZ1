4df2cc0d9732a1afc2a14cea5d25cb7c5a53ab14
kostikan <ea56@mail.ru>
2020-03-03 09:44:17 +0300

    valgrind leaks fix

--- src/DateString.c
+++ src/DateString.c
@@ -18,7 +18,7 @@ size_t getDateStringsReg(StringData *pStrings, StringData *pResStrings) {
     size_t size = 8;
     pResStrings->data = (char**)malloc(size * sizeof(char *));
     if(pResStrings->data == NULL)
-        return NULL;
+        return 0;
     size_t c = 0;
     int resCount = 0;
     regex_t regex;
@@ -35,20 +35,20 @@ size_t getDateStringsReg(StringData *pStrings, StringData *pResStrings) {
                 size *= 2;
                 pResStrings->data = (char**)realloc(pResStrings->data,size * sizeof(char*));
                 if(pResStrings->data == NULL)
-                    return NULL;
+                    return 0;
             }
             int i = 0 ;
             size_t lineSize = 128;
             pResStrings->data[resCount] = (char*)calloc(sizeof(char),lineSize);
             if(pResStrings->data[resCount] == NULL)
-                return NULL;
+                return 0;
             while(p[i] != '\0'){
                 if(i == lineSize) {
                     lineSize *= 2;
                     pResStrings->data[resCount] = (char *) realloc(pResStrings->data[resCount], lineSize * sizeof(char ));
                 }
                 if( pResStrings->data[resCount]  == NULL)
-                    return NULL;
+                    return 0;
                 pResStrings->data[resCount][i] = pStrings->data[c][i];
                 ++i;
             }
@@ -64,12 +64,12 @@ size_t getDateStringsReg(StringData *pStrings, StringData *pResStrings) {
 
 size_t getStringsFromInput(StringData *pStrings)
 {
-    size_t size = 4;
+    size_t size = 8;
     pStrings->data = (char **) malloc(size * sizeof(char*));
     if(pStrings->data == NULL)
-        return NULL;
+        return 0;
     char *line = NULL;
-    size_t len = 0;
+    size_t len = 128;
     size_t count = 0;
 
     while(getline(&line, &len, stdin) != EOF){
@@ -77,12 +77,12 @@ size_t getStringsFromInput(StringData *pStrings)
             size *= 2;
             pStrings->data = (char**)realloc(pStrings->data, size * sizeof(char*));
             if(pStrings->data == NULL)
-                return NULL;
+                return 0;
         }
         size_t i = 0;
-        pStrings->data[count] = (char *) malloc(len * sizeof(char));
+        pStrings->data[count] = (char *) calloc(sizeof(char),len);
         if(pStrings->data[count] == NULL)
-            return NULL;
+            return 0;
         while(line[i] != '\n'){
             pStrings->data[count][i] = line[i];
             ++i;
@@ -91,8 +91,8 @@ size_t getStringsFromInput(StringData *pStrings)
         if(count == 5)
             break;
     }
+    free(line);
     pStrings->size = count;
-    print(pStrings->data, pStrings->size);
 }
 
 void FreeStrings(StringData *pStrings)
